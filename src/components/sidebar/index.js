d140962daf197afc3f57b0f096860cd3da7cff18
Joe <joedsonfera@hotmail.com>
2023-07-12 15:37:25 -0300

    fourth commit buttons update

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './sidebar.css';
+import SidebarButton from './sidebarButton';
+import { MdFavorite } from "react-icons/md";
+import { FaGripfire, FaPlay } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
+import { IoLibrary } from "react-icons/io5";
+import { MdSpaceDashboard } from "react-icons/md";
 
 export default function Sidebar() {
   return (
-    <div className='sidebar-container'>Sidebar</div>
+    <div className='sidebar-container'>
+      <img src='https://warehouse-camo.ingress.cmh1.psfhosted.org/ecf9e97a8ad1f143d6a0a9d13b82bdb6e014b7f0/68747470733a2f2f7365637572652e67726176617461722e636f6d2f6176617461722f63353339366439343539623064316432373231366161383238666364383061343f73697a653d323235' className='profile-image' alt='profile' />
+      <div>
+        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
+        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
+        <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
+        <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite />} />
+        <SidebarButton title="Library" to="/library" icon={<IoLibrary />} />
+      </div>
+      <SidebarButton title="Sing Out" to="" icon={<FaSignOutAlt />} />
+    </div>
   )
 }
